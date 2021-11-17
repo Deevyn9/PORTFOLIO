bc757891a432460892c348cd5ddff1a22690734c
Divine Obeten <deevynobeeten@gmail.com>
2021-11-17 14:38:02 +0100

    Updating navbar style

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navBar = document.getElementsByClassName("navbar")[0];
+
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle("close");
     document.body.classList.toggle("black");
     navbarLinks.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 780) {
+    navBar.style.position = "sticky";
+    navBar.style.backgroundColor = "black";        
+    } else {
+    navBar.style.position = "absolute";
+    navBar.style.backgroundColor = "var(--nav-bg)";
+    }
 });

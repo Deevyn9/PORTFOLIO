27af69ff79d02513fb93153d388afd6398f3d52a
Divine Obeten <deevynobeeten@gmail.com>
2021-11-22 21:44:07 +0100

    debbuged navbar and updated social media link

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navBar = document.getElementsByClassName("navbar")[0];
-
+const linkBtn = document.querySelectorAll(".nav-link")[0];
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle("close");
@@ -9,11 +9,17 @@ toggleButton.addEventListener('click', () => {
 });
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 780) {
+    if (window.scrollY > 770) {
     navBar.style.position = "sticky";
-    navBar.style.backgroundColor = "black";        
-    } else {
+    navBar.style.backgroundColor = "black";
+    }else{
     navBar.style.position = "absolute";
     navBar.style.backgroundColor = "var(--nav-bg)";
     }
 });
+
+function hideUl() {
+    document.body.classList.toggle("close");
+    navbarLinks.classList.toggle('active');
+    document.body.classList.toggle("black");
+}

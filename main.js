fbc89e9dd6fd22b3621c0fb6d7a9dd15e3770b8d
unknown <deevynobeten@gmail.com>
2022-06-07 10:49:54 +0100

    updated resume

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navBar = document.getElementsByClassName("navbar")[0];
 const linkBtn = document.querySelectorAll(".nav-link")[0];
 
